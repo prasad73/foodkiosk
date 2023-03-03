174392dbe0af0ac32cf65f80cc0619772f1f62bd
abhi-verifygn <104504171+abhi-verifygn@users.noreply.github.com>
2023-03-03 23:13:02 +0530

    Working file with Kiosk-2

--- Kiosk-2/DIsplay Script/PySerial.py
+++ Kiosk-2/DIsplay Script/PySerial.py
@@ -6,7 +6,8 @@ import time
 from urllib.request import urlopen
 
 Open_door_place_order = [False, False, False, False, False, False, False, False, False, False, False, False]
-Open_door_pick_order = [False, False, False, False, False, False, False, False, False, False, False, False]
+Open_door_pick_order = [True, True, True, True, True, True, True, True, True, True, True, True]
+is_Occupied = [False, False, False, False, False, False, False, False, False, False, False, False]
 
 url = "https://locker-api.versicles.com/locker/locker@lulumall.json"
 
@@ -19,98 +20,108 @@ except serial.SerialException as var:
 else:
     print("Serial port Opened @ COM4")
 
+response = urlopen(url)
+data_json = json.loads(response.read())
+for i in range(0,12):
+    is_Occupied[i] = data_json['locker_boxes'][i]['is_locked']
+
 while True:
-
         try:
-                time.sleep(2)
-                response = urlopen(url)
-                data_json = json.loads(response.read())
+            time.sleep(2)
+            response = urlopen(url)
+            data_json = json.loads(response.read())
 
-                for i in range(0,12):
-                    boxNumber = str(data_json['locker_boxes'][i]['number'])
-                    state = data_json['locker_boxes'][i]['is_locked']
-                    order_status = data_json['locker_boxes'][i]['order']
-                    if(state == True):
-                        if order_status is None:
-                            if(Open_door_place_order[i] == False):
-                                Open_door_place_order[i] = True
-                                Open_door_pick_order[i] = False
-                                if(boxNumber == '1'):
-                                    connection.write(b'J')
-                                    print('J')                                    
-                                elif(boxNumber == '2'):
-                                    connection.write(b'G')
-                                    print('G')
-                                elif(boxNumber == '3'):
-                                    connection.write(b'D')
-                                    print('D')
-                                elif(boxNumber == '4'):
-                                    connection.write(b'A')
-                                    print('A')
-                                elif(boxNumber == '5'):
-                                    connection.write(b'K')
-                                    print('K')
-                                elif(boxNumber == '6'):
-                                    connection.write(b'H')
-                                    print('H')
-                                elif(boxNumber == '7'):
-                                    connection.write(b'E')
-                                    print('E')
-                                elif(boxNumber == '8'):
-                                    connection.write(b'B')
-                                    print('B')
-                                elif(boxNumber == '9'):
-                                    connection.write(b'L')
-                                    print('L')
-                                elif(boxNumber == '10'):
-                                    connection.write(b'I')
-                                    print('I')
-                                elif(boxNumber == '11'):
-                                    connection.write(b'F')
-                                    print('F')
-                                elif(boxNumber == '12'):
-                                    connection.write(b'C')
-                                    print('C')
-                        else:
-                            if(Open_door_pick_order[i] == False):
-                                Open_door_pick_order[i] = True
-                                Open_door_place_order[i] = False
-                                if(boxNumber == '1'):
-                                    connection.write(b'j')
-                                    print('j')
-                                elif(boxNumber == '2'):
-                                    connection.write(b'g')
-                                    print('g')
-                                elif(boxNumber == '3'):
-                                    connection.write(b'd')
-                                    print('d')
-                                elif(boxNumber == '4'):
-                                    connection.write(b'a')
-                                    print('a')
-                                elif(boxNumber == '5'):
-                                    connection.write(b'k')
-                                    print('k')
-                                elif(boxNumber == '6'):
-                                    connection.write(b'h')
-                                    print('h')
-                                elif(boxNumber == '7'):
-                                    connection.write(b'e')
-                                    print('e')
-                                elif(boxNumber == '8'):
-                                    connection.write(b'b')
-                                    print('b')
-                                elif(boxNumber == '9'):
-                                    connection.write(b'l')
-                                    print('l')
-                                elif(boxNumber == '10'):
-                                    connection.write(b'i')
-                                    print('i')
-                                elif(boxNumber == '11'):
-                                    connection.write(b'f')
-                                    print('f')
-                                elif(boxNumber == '12'):
-                                    connection.write(b'c')
-                                    print('c')
+            for i in range(0,12):
+                
+                if(is_Occupied[i] == True):
+                    Open_door_pick_order[i] = data_json['locker_boxes'][i]['is_locked']
+                else:
+                    Open_door_place_order[i] = data_json['locker_boxes'][i]['is_locked']
+                        
+                boxNumber = str(data_json['locker_boxes'][i]['number'])
+                    
+                if(is_Occupied[i] == False):
+                    if(Open_door_place_order[i] == True):
+                        Open_door_place_order[i] = False
+                        is_Occupied[i] = True
+                        if(boxNumber == '1'):
+                            connection.write(b'J')
+                            print('J')                                    
+                        elif(boxNumber == '2'):
+                            connection.write(b'G')
+                            print('G')
+                        elif(boxNumber == '3'):
+                            connection.write(b'D')
+                            print('D')
+                        elif(boxNumber == '4'):
+                            connection.write(b'A')
+                            print('A')
+                        elif(boxNumber == '5'):
+                            connection.write(b'K')
+                            print('K')
+                        elif(boxNumber == '6'):
+                            connection.write(b'H')
+                            print('H')
+                        elif(boxNumber == '7'):
+                            connection.write(b'E')
+                            print('E')
+                        elif(boxNumber == '8'):
+                            connection.write(b'B')
+                            print('B')
+                        elif(boxNumber == '9'):
+                            connection.write(b'L')
+                            print('L')
+                        elif(boxNumber == '10'):
+                            connection.write(b'I')
+                            print('I')
+                        elif(boxNumber == '11'):
+                            connection.write(b'F')
+                            print('F')
+                        elif(boxNumber == '12'):
+                            connection.write(b'C')
+                            print('C')
+
+                if(is_Occupied[i] == True):
+                    if (Open_door_pick_order[i] == False):
+                        Open_door_pick_order[i] = True
+                        is_Occupied[i] = False
+                        if(boxNumber == '1'):
+                            connection.write(b'j')
+                            print('j')
+                        elif(boxNumber == '2'):
+                            connection.write(b'g')
+                            print('g')
+                        elif(boxNumber == '3'):
+                            connection.write(b'd')
+                            print('d')
+                        elif(boxNumber == '4'):
+                            connection.write(b'a')
+                            print('a')
+                        elif(boxNumber == '5'):
+                            connection.write(b'k')
+                            print('k')
+                        elif(boxNumber == '6'):
+                            connection.write(b'h')
+                            print('h')
+                        elif(boxNumber == '7'):
+                            connection.write(b'e')
+                            print('e')
+                        elif(boxNumber == '8'):
+                            connection.write(b'b')
+                            print('b')
+                        elif(boxNumber == '9'):
+                            connection.write(b'l')
+                            print('l')
+                        elif(boxNumber == '10'):
+                            connection.write(b'i')
+                            print('i')
+                        elif(boxNumber == '11'):
+                            connection.write(b'f')
+                            print('f')
+                        elif(boxNumber == '12'):
+                            connection.write(b'c')
+                            print('c')
+ 
         except KeyError:
                 print("Crosscheck Parsed JSON Data")
         except Exception as e:
