9a0d33f126be0eb09ba63a0524dc0927f648dd4b
abhishek_verifygn <abhishek.verifygntech@gmail.com>
2023-12-28 20:29:55 +0530

    Kiosk5 - online+offline mode added

--- Kiosk-5/test/ESP_switch_test/ESP_switch_test.ino
+++ Kiosk-5/test/ESP_switch_test/ESP_switch_test.ino
@@ -1,13 +1,15 @@
 void setup() {
   pinMode(23, INPUT_PULLUP);
   pinMode(22, INPUT_PULLUP);
+  pinMode(18, INPUT_PULLUP);
   Serial.begin(115200);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("22 = ");Serial.println(digitalRead(22));
-  Serial.print("23 = ");Serial.println(digitalRead(23));
+  Serial.print("22 = ");Serial.print(digitalRead(22));
+  Serial.print("  23 = ");Serial.print(digitalRead(23));
+  Serial.print("  18 = ");Serial.println(digitalRead(18));
   delay(2000);
 }
